140e054eec8926af7585a9624ebbf5889083824a
azuldev <91290695+redazul@users.noreply.github.com>
2022-12-15 01:38:12 -0500

    Update start_promtail.sh

--- start_promtail.sh
+++ start_promtail.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 wget https://github.com/grafana/loki/releases/download/v2.7.1/promtail-linux-amd64.zip
+unzip promtail-linux-amd64.zip
+wget https://raw.githubusercontent.com/redazul/devops_v2/main/promtail.yaml
+pubID=$(dig +short myip.opendns.com @resolver1.opendns.com)
+sed -i "s|- url: http://localhost:3100/loki/api/v1/push|- url: http://$pubID/loki/api/v1/push|g" promtail.yaml
+./promtail-linux-amd64 -config.file=promtail.yaml &
